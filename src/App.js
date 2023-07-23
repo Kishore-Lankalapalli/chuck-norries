f81854198007c74df7bc4ef46319bb58f7b55836
kishore <113195648+Kishore-Lankalapalli@users.noreply.github.com>
2023-07-23 11:32:20 +0000

    Update package-lock.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
-import {Component} from 'react'
+import { Component } from "react";
 
-import JokeCategoryItem from './components/JokeCategoryItem'
-import './App.css'
+import JokeCategoryItem from "../src/JokeCategoryItem";
+import "./App.css";
 
 class App extends Component {
   state = {
     categoriesList: [],
-  }
+  };
 
   componentDidMount() {
-    this.getChunkDetails()
+    this.getChunkDetails();
   }
 
   getChunkDetails = async () => {
-    const chunkUrl = 'https://api.chucknorris.io/jokes/categories'
+    const chunkUrl = "https://api.chucknorris.io/jokes/categories";
 
     const options = {
-      method: 'GET',
-    }
+      method: "GET",
+    };
 
-    const response = await fetch(chunkUrl, options)
+    const response = await fetch(chunkUrl, options);
 
-    const data = await response.json()
-    this.setState({categoriesList: data})
-  }
+    const data = await response.json();
+    this.setState({ categoriesList: data });
+  };
 
   render() {
-    const {categoriesList} = this.state
+    const { categoriesList } = this.state;
 
-    console.log(categoriesList)
+    console.log(categoriesList);
 
-    const newCategoriesList = []
+    const newCategoriesList = [];
 
     categoriesList.forEach((item, index) => {
       const newItem = {
         item,
         id: index + 1,
-      }
-      newCategoriesList.push(newItem)
-    })
+      };
+      newCategoriesList.push(newItem);
+    });
 
-    console.log(newCategoriesList)
+    console.log(newCategoriesList);
 
     return (
       <div className="bg-container">
         <h1 className="chunk-noories-title">Chuck Norries</h1>
         <ul className="categories-list-container">
-          {categoriesList.map(category => (
+          {categoriesList.map((category) => (
             <JokeCategoryItem category={category} key={category.id} />
           ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
